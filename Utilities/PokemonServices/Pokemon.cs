7bea1e006d972acf1d90bbfd97a2c63ffa9e7389
QXW0993 <Vincenzo.Gambale@partner.bmw.de>
2020-08-29 12:54:31 +0200

    Created utility class for pokemon services and implemented first method to retreive pokemon id

---
+++ Utilities/PokemonServices/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace PokemonShakespeareTranslatorAPI.Utilities.PokemonServices
+{
+	public static class Pokemon
+	{
+		public static int getPokemonId(string pokemonName)
+		{
+			string url = "http://pokeapi.co/api/v2/pokemon";
+			url = url + "/" + pokemonName;
+			var restClient = new RestClient(url);
+			var request = new RestRequest(Method.GET);
+			var response = restClient.Execute(request);
+			var obj = JsonConvert.DeserializeObject<dynamic>(response.Content);
+			return obj["id"];
+		}
+	}
+}
