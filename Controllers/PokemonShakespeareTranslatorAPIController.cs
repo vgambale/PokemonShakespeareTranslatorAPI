01e4780ee1ce243982db81d39ff6cf195989fcdd
QXW0993 <Vincenzo.Gambale@partner.bmw.de>
2020-08-29 12:36:20 +0200

    Add project files.

---
+++ Controllers/PokemonShakespeareTranslatorAPIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonShakespeareTranslatorAPI.Controllers
+{
+	[Route("pokemon/")]
+	[ApiController]
+	public class PokemonShakespeareTranslatorAPIController : ControllerBase
+	{
+		[HttpGet("{pokemonName}")]
+		public string Get(string pokemonName)
+		{
+			return pokemonName;
+		}
+	}
+}
